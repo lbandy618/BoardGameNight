4e921916c51487f3ec4bf5d41115f25817d6b083
Alex <a2weber94@gmail.com>
2022-04-11 11:14:28 -0400

    saved eventController

--- BoardGameNight/Controllers/EventController.cs
+++ BoardGameNight/Controllers/EventController.cs
@@ -1,6 +1,8 @@
 ﻿using BoardGameNight.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace BoardGameNight.Controllers
 {
@@ -20,6 +22,29 @@ namespace BoardGameNight.Controllers
        public Event getEventByID(int eventID)
         {
             return this.context.Events.Where(e => e.Id == eventID).FirstOrDefault();
+        }
+
+        [HttpGet("eventByDate")]
+        public List<Event> getEventsByDate(DateTime date)
+        {
+            return this.context.Events.Where(e => e.Date == date).ToList();
+        }
+
+        [HttpGet("eventByAttendeeID")]
+        public List<Event> getEventsByAttendeeID(int userID)
+        {
+            List<SessionAttendee> sessionAttendees = this.context.SessionAttendees.Where(a => a.UserId == userID).ToList();
+            List<Session> sessionsAttended = new List<Session>();
+            foreach (SessionAttendee sessionAttendee in sessionAttendees)
+            {
+                sessionsAttended.Add(this.context.Sessions.Find(sessionAttendee.SessionId));
+            }
+            List<Event> events = new List<Event>();
+            foreach (Session session in sessionsAttended)
+            {
+                events.AddRange(this.context.Events.Include(e => e.Session.SessionAttendees).Where(e => e.SessionId == session.Id));
+            }
+            return events;
         }
 
         [HttpPost]
