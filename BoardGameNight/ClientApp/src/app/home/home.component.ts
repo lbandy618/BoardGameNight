0c73b2f9d4f42a19bdeb7f34f1d2caed119983e2
Jon <JJKing619@gmail.com>
2022-04-14 12:02:01 -0400

    added selectCategory menu

--- BoardGameNight/ClientApp/src/app/home/home.component.ts
+++ BoardGameNight/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { concat } from 'rxjs';
 import { ApiGame, GameElement } from '../ApiGame';
 import { BoardgameapiService } from '../boardgameapi.service';
+import { Category, iCategory } from '../category';
 import { GameShelf } from '../game-shelf';
 import { GameShelfService } from '../game-shelf.service';
 import { User } from '../user';
@@ -16,8 +17,12 @@ import { UserService } from '../user.service';
 
   users:User[] = [];
   selectedUsers:User[]=[];
-  selectedUsersGameShelf: GameShelf[] =[]
+  selectedUsersGameShelf: GameShelf[] =[];
   displayPeople:boolean = false;
+  selectedCategoryIds:string[] = [];
+  selectedCategoryNames:string[] =[];
+  displayCategoryNames:boolean = false;
+  chosenCategory:iCategory = {} as iCategory;
 
   constructor(private userService: UserService, private gameShelfService: GameShelfService, private boardGameApiService: BoardgameapiService) { }
 
@@ -45,9 +50,21 @@ import { UserService } from '../user.service';
     console.log(this.selectedUsers);
   }
 
+  selectCategory(category:string){
+    this.chosenCategory = Category.getObjectByName(category);
+    console.log(this.chosenCategory);
+
+    // let index:number = this.selectedCategoryNames.findIndex(c => c.)
+  }
+
   togglePeople(){
     this.displayPeople = !this.displayPeople;
   }
+
+  toggleCategory(){
+    this.displayCategoryNames = !this.displayCategoryNames;
+  }
+
 
   getSelectedUserOwnedGames(){
     this.selectedUsers.forEach(u=>{
@@ -55,6 +72,12 @@ import { UserService } from '../user.service';
       console.log(response);
       this.selectedUsersGameShelf = this.selectedUsersGameShelf.concat(response)
       })
+    })
+  }
+
+  getSelectedCategory(){
+    this.selectedCategoryNames.forEach(c =>{
+      
     })
   }
 
@@ -67,29 +90,45 @@ import { UserService } from '../user.service';
       console.log(selectedGameIds)
       let sortedArray:string[] = selectedGameIds.sort();
       let results:string[] = [];
-      console.log(sortedArray)
       for(let i = 0; i < sortedArray.length - 1; i++){
         if(sortedArray[i+1] == sortedArray[i]){
           results.push(sortedArray[i]);
         }
       }
         console.log(results);
-        console.log(sortedArray)
+        console.log("this is showing only dups");
+        if(results.length == 0){ //no dubs found
+          results = selectedGameIds;
+        }
         this.boardGameApiService.getBoardGameByID(results).subscribe((response:ApiGame)=>{
         console.log(response);
+        console.log("showing full api response");
         let categoryIds:string[] = []
         response.games.forEach(g =>{  //nested for each, grabbing the category id out of category array inside each game list
-            console.log(response.games)
+            console.log(response.games);
+            console.log("grabbed cat id out of cat array")
             g.categories.forEach(c => {
             categoryIds.push(c.id);
         })
+        console.log(categoryIds);
+        this.selectedCategoryIds = categoryIds;
+        this.getBoardGameCategoriesNames();
       })
     })
   }
 
-//getBoardGameCategoriesByID
-  // getNumberOfPlayers(form:NgForm){
+  getBoardGameCategoriesNames(){
+    this.selectedCategoryIds.forEach(c => { 
+    this.selectedCategoryNames.push(Category.getCategoryNameById(c));
+    })
+    this.selectedCategoryNames = Array.from(new Set(this.selectedCategoryNames));
+    console.log(this.selectedCategoryNames);
+  }
 
-  // }
+
+  
+    // getNumberOfPlayers(form:NgForm){
+
+    // }
 
 }
